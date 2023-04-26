07443a0249b3931b8e4e25f28db12c193a63a36a
ALI DUGGAN <ali_duggan@hotmail.com>
2023-04-26 14:53:16 +1000

    Update(ticket x)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useRef, useEffect, useState } from 'react';
 import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
 
+//npm install papaparse
+import Papa from 'papaparse';
+
 //my map token:
 mapboxgl.accessToken = 'pk.eyJ1IjoiYWxpZHVnIiwiYSI6ImNsZTIydjY1cjBtdHYzdm12ZnpvMTZjMWoifQ.-Gc6QMOJQp4BoIYao2_kPg';
 
@@ -9,9 +12,15 @@ export default function App() {
     //initial display position of the map:
     const mapContainer = useRef(null);
     const map = useRef(null);
+
     const [lng, setLng] = useState(140.512155);
     const [lat, setLat] = useState(-20.669637);
     const [zoom, setZoom] = useState(13.19);
+
+    //import waypoints from csv
+    const [waypoints, setWaypoints] = useState([]);
+
+  
 
     //initialise the map
     useEffect(() => {
@@ -22,16 +31,66 @@ export default function App() {
             center: [lng, lat],
             zoom: zoom
         });
-    });
+
+        //import the waypoints from your csv
+        Papa.parse('https://drive.google.com/file/d/1Er2a_BlLyKQqJ6wzF_jCd8ahfOHAxLIV/view?usp=share_link', {
+            download: true,
+            header: true,
+            complete: (results) => {
+                const waypoints = results.data.map((waypoint) => ({
+                    type: 'Feature',
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [parseFloat(waypoint.Latitude), parseFloat(waypoint.Longitude)]
+                    },
+                    properties: {
+                        name: waypoint.Name
+                    }
+                }));
+                console.log("Imported Waypoints:", waypoints); // print the imported waypoints
+
+
+        
+                setWaypoints(waypoints);
+
+                map.current.on('load', () => {
+                    map.current.addSource('waypoints', {
+                        type: 'geojson',
+                        data: {
+                            type: 'FeatureCollection',
+                            features: waypoints
+                        }
+                    });
+
+                    map.current.addLayer({
+                        id: 'waypoints',
+                        type: 'circle',
+                        source: 'waypoints',
+                        paint: {
+                            'circle-color': '#FFA500',
+                            'circle-radius': 50,
+                            'circle-stroke-width': 1,
+                            'circle-stroke-color': '#fff'
+                        }
+                    });
+                });
+            }
+        });
+    }, []);
+
     //enable interactive map:
     useEffect(() => {
         if (!map.current) return; // wait for map to initialize
+
         map.current.on('move', () => {
             setLng(map.current.getCenter().lng.toFixed(4));
             setLat(map.current.getCenter().lat.toFixed(4));
             setZoom(map.current.getZoom().toFixed(2));
         });
     });
+
+
+
     //render map in the app! :
     return (
         <div>
@@ -40,5 +99,5 @@ export default function App() {
             </div>
             <div ref={mapContainer} className="map-container" />
         </div>
-    );
+    );  
 }
